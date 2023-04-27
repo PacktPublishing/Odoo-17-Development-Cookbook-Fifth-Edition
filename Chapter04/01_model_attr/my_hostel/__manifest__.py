003badca86139ed34af3d7679020c7128aca1d96
Maitree Trivedi <m.trivedi.serpentcs@gmail.com>
2023-04-27 19:16:39 +0530

    [ADD]added module hostel management.

---
+++ Chapter04/01_model_attr/my_hostel/__manifest__.py
@@ -0,0 +1,24 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Hostel Management",
+    "summary": "Manage Hostel easily",
+    "description": """
+Manage Hostel
+==============
+Efficiently manage the entire residential facility in the school
+    """,
+    "version": "15.0.1.0.0",
+    "author": "Serpent Consulting Services Pvt. Ltd.",
+    "category": "Tools",
+    "website": "http://www.serpentcs.com",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": [
+        "security/hostel_security.xml",
+        "security/ir.model.access.csv",
+        "views/hostel.xml"
+    ],
+    "demo": [],
+    "installable": True,
+}
