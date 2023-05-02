70241b1ec508a50da11272ec577f8ed9260bc798
Vacha Bhatt <vacha.bhatt@serpentcs.com>
2023-05-02 18:10:26 +0530

    [ADD]Chaptor05: Basic Server-Side Development

---
+++ Chapter05/06_search_on_model/my_hostel/models/hostel_room_category.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class RoomCategory(models.Model):
+    _name = 'hostel.room.category'
+
+    name = fields.Char('Category')
+    description = fields.Text('Description')
+    parent_id = fields.Many2one(
+        'hostel.room.category',
+        string='Parent Category',
+        ondelete='restrict',
+        index=True
+    )
+    child_ids = fields.One2many(
+        'hostel.room.category', 'parent_id',
+        string='Child Categories')
