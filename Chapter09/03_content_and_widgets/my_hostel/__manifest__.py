ae1a18d240c2b9376a6b00345aec2a6449765afc
Rajan-SCS <rajan.s.serpentcs@gmail.com>
2023-08-23 19:35:34 +0530

    [ADD] Added Chapter09 and add topics 1 to 9.

---
+++ Chapter09/03_content_and_widgets/my_hostel/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My Hostel",  # Module title
+    'summary': "Manage Hostel easily",  # Module subtitle phrase
+    'description': """
+Long description
+    """,  # Supports reStructuredText(RST) format
+    "version": "15.0.1.0.0",
+    "author": "Serpent Consulting Services Pvt. Ltd.",
+    "category": "Uncategorized",
+    "website": "http://www.serpentcs.com",
+    "depends": ['base'],
+    "license": "AGPL-3",
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/my_hostel.xml',
+    ],
+}
