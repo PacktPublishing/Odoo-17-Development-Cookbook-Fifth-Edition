65b2050117482deb04dd1f012c010d2ec086fbd5
Rajan-SCS <rajan.s.serpentcs@gmail.com>
2023-08-25 18:11:16 +0530

    [IMP] Improved the code of Chapter09.

--- Chapter09/23_map_view/my_hostel/models/hostel_room_category.py
+++ Chapter09/23_map_view/my_hostel/models/hostel_room_category.py
@@ -24,6 +24,7 @@ class RoomCategory(models.Model):
     related_hostel_room = fields.Integer(compute='_compute_related_hostel_room')
     date_end = fields.Datetime(string='Ending Date', index=True, copy=False)
     date_assign = fields.Datetime(string='Assigning Date', copy=False,)
+    address = fields.Char(strin="Address")
 
     def _compute_related_hostel_room(self):
         for record in self:
