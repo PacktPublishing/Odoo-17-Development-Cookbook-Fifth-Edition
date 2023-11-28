a552f47697d7a57ea5047b9303a83719cf8de474
Maitree Trivedi <m.trivedi.serpentcs@gmail.com>
2023-11-28 11:42:02 +0530

    [FIX]Fixed module installtion issue.

--- Chapter13/04_path_override/website_ext/controllers/main.py
+++ Chapter13/04_path_override/website_ext/controllers/main.py
@@ -7,9 +7,7 @@ class WebsiteInfo(Website):
     @http.route()
     def website_info(self):
         result = super(WebsiteInfo, self).website_info()
-        print("\n \n result======>>>", result.qcontext)
         result.qcontext['apps'] = result.qcontext['apps'].filtered(
             lambda x: x.name != 'website'
         )
-        print("\n \n result=$$$$$=====>>>", result.qcontext)
         return result
