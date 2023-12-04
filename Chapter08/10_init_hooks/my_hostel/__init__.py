8839332540641cb684311889409629c3a15ee3e6
Rajan-SCS <rajan.s.serpentcs@gmail.com>
2023-12-04 12:32:24 +0530

    [MIG] Migrated v16 to v17.

--- Chapter08/10_init_hooks/my_hostel/__init__.py
+++ Chapter08/10_init_hooks/my_hostel/__init__.py
@@ -3,11 +3,8 @@ from . import models
 from . import controllers
 from . import wizard
 
-from odoo import api, SUPERUSER_ID
 
-
-def add_room_hook(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
+def add_room_hook(env):
     room_data1 = {'name': 'Room 1', 'room_no': '01'}
     room_data2 = {'name': 'Room 2', 'room_no': '02'}
     env['hostel.room'].create([room_data1, room_data2])
