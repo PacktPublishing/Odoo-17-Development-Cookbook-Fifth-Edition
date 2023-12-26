58188819cfb529268e54e042f03c1121b78ddc85
Chirag Patel <chirag.patel.serpentcs@gmail.com>
2023-12-26 12:10:12 +0530

    [IMP] Improved code for ch14 topic 2.

--- Chapter14/02_display_datas/my_hostel/__manifest__.py
+++ Chapter14/02_display_datas/my_hostel/__manifest__.py
@@ -16,7 +16,7 @@ Display Hostel records in the web page.
         "security/hostel_security.xml",
         "security/ir.model.access.csv",
         "views/hostel.xml",
-        "views/hostel_templates.xml",
+        "views/hostel_template.xml",
         "views/custom_template.xml",
     ],
     # This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
